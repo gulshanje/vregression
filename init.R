47ded1c7b67a4898d05cd22985a847b7f97758b4
Gulshan <shrigulshankumar@gmail.com>
2021-08-03 19:29:17 +0300

    Added excel sheet

--- init.R
+++ init.R
@@ -3,21 +3,21 @@
 # # Example R code to install packages if not already installed
 # #
 # 
-# my_packages = c('shiny','dplyr','DT','data.table','shinythemes','readxl','openxlsx','datasets','shinydashboard','ggplot2','tidyr','devtools')
-# 
-# install_if_missing = function(p) {
-#   if (p %in% rownames(installed.packages()) == FALSE) {
-#     install.packages(p)
-#   }
-# }
-# 
-# invisible(sapply(my_packages, install_if_missing))
+my_packages = c('shiny','dplyr','DT','data.table','shinythemes','readxl','openxlsx','datasets','shinydashboard','ggplot2','tidyr','devtools')
 
-
-for (package in c('shiny','DT','data.table','shinythemes','readxl','openxlsx','datasets','shinydashboard','ggplot2','tidyr','devtools',
-                  'lubridate')) {
-  if (!require(package, character.only=T, quietly=T)) {
-    install.packages(package,repos="http://cran.us.r-project.org")
-    library(package, character.only=T)
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
   }
 }
+
+invisible(sapply(my_packages, install_if_missing))
+
+
+# for (package in c('shiny','DT','data.table','shinythemes','readxl','openxlsx','datasets','shinydashboard','ggplot2','tidyr','devtools',
+#                   'lubridate')) {
+#   if (!require(package, character.only=T, quietly=T)) {
+#     install.packages(package,repos="http://cran.us.r-project.org")
+#     library(package, character.only=T)
+#   }
+# }
