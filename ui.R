f090250b82cf671a973f01150ba90098d93bf237
Gulshan <shrigulshankumar@gmail.com>
2021-08-03 18:33:51 +0300

    Added readxl depend

--- ui.R
+++ ui.R
@@ -1,38 +1,42 @@
-library(shiny)
 
-# Training set
-TrainSet <- read.csv("training.csv", header = TRUE)
-TrainSet <- TrainSet[,-1]
-
-pageWithSidebar(
-
-  # Page header
-  headerPanel('Iris Predictor'),
-
-  # Input values
-  sidebarPanel(
-    HTML("<h3>Input parameters</h4>"),
-    sliderInput("Sepal.Length", label = "Sepal Length", value = 5.0,
-                min = min(TrainSet$Sepal.Length),
-                max = max(TrainSet$Sepal.Length)
-    ),
-    sliderInput("Sepal.Width", label = "Sepal Width", value = 3.6,
-                min = min(TrainSet$Sepal.Width),
-                max = max(TrainSet$Sepal.Width)),
-    sliderInput("Petal.Length", label = "Petal Length", value = 1.4,
-                min = min(TrainSet$Petal.Length),
-                max = max(TrainSet$Petal.Length)),
-    sliderInput("Petal.Width", label = "Petal Width", value = 0.2,
-                min = min(TrainSet$Petal.Width),
-                max = max(TrainSet$Petal.Width)),
-
-    actionButton("submitbutton", "Submit", class = "btn btn-primary")
-  ),
-
-  mainPanel(
-    tags$label(h3('Status/Output')), # Status/Output Text Box
-    verbatimTextOutput('contents'),
-    tableOutput('tabledata') # Prediction results table
-
-  )
+navbarPage("Regression",
+           tabPanel("Plot",
+                    plotOutput("distPlot")
+                    
+           ),
+           tabPanel("Report",
+                    
+           ),
+           navbarMenu("Plotting",
+                      tabPanel("Model A",
+                               h1('Model A'),
+                               plotOutput("plotModelA")
+                               
+                               
+                      ),
+                      tabPanel("Model B",
+                               h1('Model B'),
+                               plotOutput("plotModelB")
+                      )
+           ), 
+           navbarMenu("Summary",
+                      tabPanel("Model A",
+                               h1('Model A'),
+                               verbatimTextOutput("summaryModelA")
+                               
+                      ),
+                      tabPanel("Model B",
+                               h1('Model B'),
+                               verbatimTextOutput("summaryModelB")
+                      )
+           ), 
+           navbarMenu("Data",
+                      tabPanel("Table",
+                               dataTableOutput('dataa'),
+                               
+                      ),
+                      tabPanel("About",
+                               verbatimTextOutput("shapiroTest"),
+                      )
+           )
 )
