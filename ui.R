73d218ec80e5a99e050f739c8bf14eb537c13509
Gulshan <shrigulshankumar@gmail.com>
2021-08-03 18:37:32 +0300

    Removed datatable

--- ui.R
+++ ui.R
@@ -10,33 +10,33 @@ navbarPage("Regression",
            navbarMenu("Plotting",
                       tabPanel("Model A",
                                h1('Model A'),
-                               plotOutput("plotModelA")
+                               
                                
                                
                       ),
                       tabPanel("Model B",
                                h1('Model B'),
-                               plotOutput("plotModelB")
+                               
                       )
            ), 
            navbarMenu("Summary",
                       tabPanel("Model A",
                                h1('Model A'),
-                               verbatimTextOutput("summaryModelA")
+                                
                                
                       ),
                       tabPanel("Model B",
                                h1('Model B'),
-                               verbatimTextOutput("summaryModelB")
+                               
                       )
            ), 
            navbarMenu("Data",
                       tabPanel("Table",
-                               dataTableOutput('dataa'),
+                                
                                
                       ),
                       tabPanel("About",
-                               verbatimTextOutput("shapiroTest"),
+                                
                       )
            )
 )
