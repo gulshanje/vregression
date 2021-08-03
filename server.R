a899a4435f9f1e82fb4bc95b198ade9fa0e64c5c
Gulshan <shrigulshankumar@gmail.com>
2021-08-04 01:16:33 +0300

    ADDED plot

--- server.R
+++ server.R
@@ -31,5 +31,39 @@ shinyServer(function(input, output, session) {
   
     hist(datapks, col="coral4", border = 'white')
   })
+  
+  output$plotModelA <- renderPlot({
+    pointData <- data.frame(helloKittyData())
+    log_PKS <- (log(pointData$PKS, 10))
+    modelA <- lm(log_PKS ~ VAESTONKASVU + TALOUSKASVU + EU28_hintaindeksi + Uudisrakentaminen  , data = pointData)
+    windows.options(width=100, height=100)
+    
+    par(mfrow = c(2,2))
+    plot (modelA,col =  c("#00AFBB", "#E7B800", "#FC4E07"),
+          pch = c(16, 17, 18) )
+    
+  },  height = 750, width = 900)
+  
+  output$plotModelB <- renderPlot({
+    pointData <- data.frame(helloKittyData())
+    modelB <- lm(PKS~  VAESTONKASVU + TALOUSKASVU + EU28_hintaindeksi + Uudisrakentaminen  , data = pointData)
+    summary(modelB)
+    par(mfrow = c(2,2))
+    plot (modelB,col =  c("#00AFBB"),
+          pch = c(18) )
+  },  height = 900, width = 900)
+  
+  # output$summaryModelA <- renderPrint({
+  #   data <- data.frame(helloKittyData())
+  #   log_PKS <- (log(data$PKS, 10))
+  #   modelOne <- lm(log_PKS~ VAESTONKASVU + TALOUSKASVU + EU28_hintaindeksi + Uudisrakentaminen  , data = data)
+  #   summary(modelOne)
+  # })
+  # output$summaryModelB <- renderPrint({
+  #   data <- data.frame(helloKittyData())
+  #   modelTwo <- lm(PKS~  VAESTONKASVU + TALOUSKASVU + EU28_hintaindeksi + Uudisrakentaminen  , data = data)    
+  #   summary(modelTwo)
+  # })
+  
 
 })
