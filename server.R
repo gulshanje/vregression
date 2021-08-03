ddeeaf7987e2ed23820789a422ed883fc103e0e7
Gulshan <shrigulshankumar@gmail.com>
2021-08-04 00:44:18 +0300

    Calling readxl library

--- server.R
+++ server.R
@@ -18,5 +18,66 @@ shinyServer(function(input, output, session) {
   output$dataa <- renderDataTable({
     helloKittyData()
   })
-
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    data <- data.frame(helloKittyData())
+    datapks <- (log(data$PKS, 10))
+    #print(datapks)
+    
+    abline(v = mean(datapks),
+           lwd = 2,      # Thickness
+           lty = 2)      # Dashed line
+  
+    hist(datapks, col="coral4", border = 'white')
+  })
+  
+  output$plotModelA <- renderPlot({
+    pointData <- data.frame(helloKittyData())
+    log_PKS <- (log(pointData$PKS, 10))
+    modelA <- lm(log_PKS ~ VAESTONKASVU + TALOUSKASVU + EU28_hintaindeksi + Uudisrakentaminen  , data = pointData)
+    windows.options(width=100, height=100)
+    
+    par(mfrow = c(2,2))
+    plot (modelA,col =  c("#00AFBB", "#E7B800", "#FC4E07"),
+          pch = c(16, 17, 18) )
+    
+  },  height = 750, width = 900)
+  
+  output$plotModelB <- renderPlot({
+    pointData <- data.frame(helloKittyData())
+    modelB <- lm(PKS~  VAESTONKASVU + TALOUSKASVU + EU28_hintaindeksi + Uudisrakentaminen  , data = pointData)
+    summary(modelB)
+    par(mfrow = c(2,2))
+    plot (modelB,col =  c("#00AFBB"),
+          pch = c(18) )
+  },  height = 900, width = 900)
+  
+  output$summaryModelA <- renderPrint({
+    data <- data.frame(helloKittyData())
+    log_PKS <- (log(data$PKS, 10))
+    modelOne <- lm(log_PKS~ VAESTONKASVU + TALOUSKASVU + EU28_hintaindeksi + Uudisrakentaminen  , data = data)
+    summary(modelOne)
+  })
+  output$summaryModelB <- renderPrint({
+    data <- data.frame(helloKittyData())
+    modelTwo <- lm(PKS~  VAESTONKASVU + TALOUSKASVU + EU28_hintaindeksi + Uudisrakentaminen  , data = data)    
+    summary(modelTwo)
+  })
+  
+  output$shapiroTest <- renderValueBox({
+    data <- data.frame(helloKittyData())
+    
+    shapiroResult <- shapiro.test(data$PKS)
+    ShapiroPValue <- shapiroResult$p.value
+    print(shapiroResult$p.value)
+    print(shapiroResult$statistic)
+    getLog <- log(data$PKS,10)
+    print('get log ',getLog)
+    
+    valueBox(ShapiroPValue,"Shapiro Result", 
+             color = 'light-blue') 
+    
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 
-navbarPage("Regression",
-           tabPanel("Plot",
+navbarPage("L-Regression",
+           tabPanel("Home",
                     plotOutput("distPlot")
                     
            ),
@@ -10,24 +10,24 @@ navbarPage("Regression",
            navbarMenu("Plotting",
                       tabPanel("Model A",
                                h1('Model A'),
-                               
+                               plotOutput("plotModelA")
                                
                                
                       ),
                       tabPanel("Model B",
                                h1('Model B'),
-                               
+                               plotOutput("plotModelB")
                       )
            ), 
            navbarMenu("Summary",
                       tabPanel("Model A",
                                h1('Model A'),
-                                
+                               verbatimTextOutput("summaryModelA")
                                
                       ),
                       tabPanel("Model B",
                                h1('Model B'),
-                               
+                               verbatimTextOutput("summaryModelB")
                       )
            ), 
            navbarMenu("Data",
@@ -36,7 +36,12 @@ navbarPage("Regression",
                                
                       ),
                       tabPanel("About",
-                                
+                               h1('Talous'),
+                               h3('Group Member'),
+                               p('Valtteri'),
+                               p('Gulshan'),
+                               p('Martti'),
+                               p('Tiina')
                       )
            )
 )
